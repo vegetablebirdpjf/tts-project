4fb46efe14f2eaa14d5e1c8fa97831c1085e43e9
pjf <2640388029@qq.com>
2026-03-01 17:23:54 +0800

    修改vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',  // 允许外部访问
     port: 5173,        // 自定义端口
-    strictPort: true
+    strictPort: true,
+    allowedHosts: ['xxmmllqqqqccqq.xyz', 'www.xxmmllqqqqccqq.xyz']
   }
 })
